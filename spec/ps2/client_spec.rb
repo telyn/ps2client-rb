e40e556994aa406b70eba536defd133010fdecda
Telyn Z <175827+telyn@users.noreply.github.com>
2020-09-20 02:51:41 +0100

    remove silly failing test

--- spec/ps2/client_spec.rb
+++ spec/ps2/client_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Ps2::Client do
   it "has a version number" do
     expect(Ps2::Client::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
